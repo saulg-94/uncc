f46cc7aee1202364d0bf4d3f66db552d4dcc9253
saulg-94 <94.gonzlezsaul@gmail.com>
2024-01-05 09:03:08 -0500

    added example of allocating memory with a buffer using Buffer object exposed from nodeJS API;

---
+++ buffers/buffersInAction.js
@@ -0,0 +1,26 @@
+import {Buffer} from 'buffer';
+
+
+
+// const memoryContainer = Buffer.alloc(4) // 4 bytes (32 bits)
+// EXAMPLES of other methods of storing data memoryContainer.write... GO TO following link for more on all Buffer METHODS : https://nodejs.org/docs/latest/api/buffer.html
+
+
+
+// EXAMPLE ---- EXAMPLE ---- EXAMPLE ---- EXAMPLE ----
+// memoryContainer[3]
+
+// memoryContainer[0] = 0xf4;
+// memoryContainer[1] = 0x34;
+// memoryContainer[2] = 0xb6;
+// memoryContainer[3] = 0xff;
+
+
+// console.log(memoryContainer);
+// console.log(memoryContainer[0]);
+// console.log(memoryContainer[1]);
+// console.log(memoryContainer[2]);
+// console.log(memoryContainer[3]);
+
+// console.log(memoryContainer.toString('hex'));
+// EXAMPLE ---- EXAMPLE ---- EXAMPLE ---- EXAMPLE ----
